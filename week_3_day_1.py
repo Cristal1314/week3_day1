6c5151791a25651f46a53dce1dfd672bb7f5da6d
Jimenez <CFJimenez@cps.k12.il.us>
2024-09-10 09:25:08 -0500

    f string conatenation

---
+++ week_3_day_1.py
@@ -0,0 +1,161 @@
+# # Week3
+# # This week we will work on :
+# # Working With Strings
+
+
+# # 1.   Working With Numbers
+# # 2.   Getting Input From Users
+
+
+
+
+# # 1.   Building a Basic Calculator
+# # 2.   Mad Libs Game
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Review
+# create variables for the following :
+# 1. age
+age = 25 #integer variable
+# 2. name
+name = "John" #string variable
+# 3. song
+song = "Happy Birthday" #String variable
+# 4. food
+food = "apples" #string variable
+# 5. number
+number = 24 #integer variable
+
+
+# #now include the variables you just made print in the following...
+
+
+# Once upon a time, there was a [age] old coder named [name].
+# concatenation --- + around your variables
+print("Once upon a time, there was a " + str(age)+ " old coder named " + name + ".") 
+print("there was a number " + str(number) + " as well ")
+print("Once there was a girl that was " + str(age) + " years old. She was " + str(number) + " last year")
+#put the age and number in a new sentence
+date_of_birth = 2021
+number2 = 123
+number3 = 123.456
+number4 = 123.33
+print("Bella was born on the year " + str(date_of_birth) + " she was counting to the numbers " + str(number2) +", " + str(number3) +", "+ str(number4))
+#create a sentence of all of this abouve
+print(f"the date of birth is {date_of_birth} and the numnber is {number2} and the number is {number3} and the number is {number4} ")
+
+# [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
+print(f"{name}liker to hum the song{song} while coding/ It was so annoying that their teammates would throw{food}until {name}would stop singing.")
+
+print(f"Still, {name} was the best coder on the team and could write {number} lines of code every day. Maybe {song} was {name}’s secret power?")
+##########################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################################################################
+# The names you use when creating these labels need to follow a few rules:
+# 1. Names can not start with a number.
+# 2. There can be no spaces in the name, use _ instead.
+# 3. Can't use any of these symbols :'",<>/?|\()!@#$%^&*~-+
+# 4. It's considered best practice (PEP8) that names are lowercase.
+# 5. Avoid using the characters 'l' (lowercase letter el), 'O' (uppercase letter oh),
+#    or 'I' (uppercase letter eye) as single character variable names.
+# 6. Avoid using words that have special meaning in Python like "list" and "str"
+
+
+# Here are some exercises to practice the rules:
+
+
+# Correcting Invalid Names: Below are some invalid names. Correct them according to the rules:
+
+
+# 1st_name
+# last name
+# email@address
+# percent%
+# variable#name
+# O
+# list
+# Creating Valid Names: Create valid names for the following descriptions:
+
+
+# The first name of a person
+# The last name of a person
+# The email address of a person
+# The percentage of marks obtained by a student
+# A variable to store the number of items in a shopping cart
+
+
+
+
+# Identify Valid and Invalid Names: Identify which of the following names are valid or invalid according to the rules:
+
+
+# first_name
+# lastName
+# email_address
+# percentage
+# variable_name
+# 1_variable
+# email@address
+# percentage%
+# i
+
+
+
+
+
+
+
+
+
+
